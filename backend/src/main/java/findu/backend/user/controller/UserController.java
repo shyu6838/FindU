4a9a74ccc3628b332d2429c8e382efc0b457f821
유수환 <yooz7443@naver.com>
2026-08-13 17:46:16 +0900

    feat: 내 정보 조회 API 구현

--- backend/src/main/java/findu/backend/user/controller/UserController.java
+++ backend/src/main/java/findu/backend/user/controller/UserController.java
@@ -3,19 +3,21 @@ package findu.backend.user.controller;
 import findu.backend.user.dto.UserResponseDto;
 import findu.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{id}")
-    public UserResponseDto getUser(@PathVariable Long id){
-
-        return userService.getUser(id);
+    // 내 정보 조회
+    @GetMapping("/me")
+    public UserResponseDto getMyInfo(
+            @AuthenticationPrincipal Long userId
+    ) {
+        return userService.getUser(userId);
     }
-
 }
