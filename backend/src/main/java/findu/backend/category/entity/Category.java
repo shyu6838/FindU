a90f94c05dc0eb9381ce10f0aed58d015a503df0
유수환 <yooz7443@naver.com>
2026-08-13 18:45:14 +0900

    feat: 카테고리 조회 API 구현

---
+++ backend/src/main/java/findu/backend/category/entity/Category.java
@@ -0,0 +1,21 @@
+package findu.backend.category.entity;
+
+import findu.backend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
